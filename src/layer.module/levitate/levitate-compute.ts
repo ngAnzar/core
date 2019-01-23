82a7d650d1df3620ce100a56c94800f1e1973ab5
Vetési Zoltán <zozzz@trigon.hu>
2019-01-23 17:58:47 +0100

    fix: levitate compute

--- src/layer.module/levitate/levitate-compute.ts
+++ src/layer.module/levitate/levitate-compute.ts
@@ -36,34 +36,8 @@ export class MagicCarpet {
     public constructor(public ref: LevitateRef) {
     }
 
-    // TODO: ne itt parseoljon
     public levitate(rects: Rects): LevitatePosition {
-        const lAlign = rects.levitate.origin
-        let pA
-        let pV
-
-        if (this.ref.anchor) {
-            const cAlign = rects.anchor.origin
-            pA = calcPlacementH[`${lAlign.horizontal}-${cAlign.horizontal}`](rects.levitate, rects.anchor, rects.constraint)
-            pV = calcPlacementV[`${lAlign.vertical}-${cAlign.vertical}`](rects.levitate, rects.anchor, rects.constraint)
-        } else if (this.ref.constraint) {
-            pA = calcPlacementH[`${lAlign.horizontal}-${lAlign.horizontal}`](rects.levitate, rects.constraint, rects.constraint)
-            pV = calcPlacementV[`${lAlign.vertical}-${lAlign.vertical}`](rects.levitate, rects.constraint, rects.constraint)
-        }
-
-        let possibilities: Array<{ placements: Placement[], rect: Rect }> = []
-        for (let a of pA) {
-            for (let b of pV) {
-                let r = a.rect.intersection(b.rect)
-                if (r) {
-                    possibilities.push({
-                        placements: [a, b],
-                        rect: r
-                    })
-                    // drawRect(r, "yellow")
-                }
-            }
-        }
+        const possibilities = this.getPlacements(rects)
 
         let result = possibilities.find((item) => {
             return rects.levitate.width <= item.rect.width && rects.levitate.height <= item.rect.height
@@ -73,16 +47,50 @@ export class MagicCarpet {
             return b.rect.area - a.rect.area
         })[0]
 
-        // console.log(result.placements.map(v => v.))
-
-        // console.log(rects, result)
-
-        // let result = this.levitateRect.copy()
-        // result.top = best.rect.top
-        // result.left = best.rect.left
-        // result = best.rect.constraint(result)
-
         return getLevitatePosition(result.placements[0], result.placements[1], rects.levitate, result.rect, rects.anchor)
+    }
+
+    public getPlacements(rects: Rects) {
+        const lAlign = rects.levitate.origin
+        let levitate = rects.levitate, anchor, constraint
+        let primaryHAlign: string, primaryVAlign: string
+        let oppositeHAlign: string, oppositeVAlign: string
+
+        // TODO: jó megoldás az oppsoite oldal számításra
+
+        if (this.ref.anchor) {
+            const cAlign = rects.anchor.origin
+            anchor = rects.anchor
+            constraint = rects.constraint
+            primaryHAlign = `${lAlign.horizontal}-${cAlign.horizontal}`
+            primaryVAlign = `${lAlign.vertical}-${cAlign.vertical}`
+        } else if (this.ref.constraint) {
+            anchor = constraint = rects.constraint
+            primaryHAlign = `${lAlign.horizontal}-${lAlign.horizontal}`
+            primaryVAlign = `${lAlign.vertical}-${lAlign.vertical}`
+        }
+
+        let hPlacements = [
+            calcPlacementH[primaryHAlign](levitate, anchor, constraint)
+        ]
+
+        let vPlacements = [
+            calcPlacementH[primaryVAlign](levitate, anchor, constraint)
+        ]
+
+        let result = []
+        for (let hPlacement of hPlacements) {
+            for (let vPlacement of vPlacements) {
+                let r = hPlacement.rect.intersection(vPlacement.rect)
+                if (r) {
+                    result.push({
+                        placements: [hPlacement, vPlacement],
+                        rect: r
+                    })
+                }
+            }
+        }
+        return result
     }
 
     public dispose(): void {
@@ -102,25 +110,21 @@ type Placement = {
 }
 
 
-type Calculator = (levitate: Readonly<Rect>, target: Readonly<Rect>, constraint: Readonly<Rect>) => Placement[]
+type Calculator = (levitate: Readonly<Rect>, target: Readonly<Rect>, constraint: Readonly<Rect>) => Placement
 type Calculators = { [key: string]: Calculator }
 
 
-function placementCalculator(levitateAlign: HAlign | VAlign, targetAlign: HAlign | VAlign, orient: "H" | "V", opposit?: Calculator): Calculator {
-    return function (levitate: Readonly<Rect>, target: Readonly<Rect>, constraint: Readonly<Rect>): Placement[] {
+function placementCalculator(levitateAlign: HAlign | VAlign, targetAlign: HAlign | VAlign, orient: "H" | "V"): Calculator {
+    return function (levitate: Readonly<Rect>, target: Readonly<Rect>, constraint: Readonly<Rect>): Placement {
         let placement = constraint.copy()
         placement[levitateAlign] = target[targetAlign]
 
-        let res: Placement[] = [{
+        return {
             rect: constraint.constraint(placement),
             levitate: levitateAlign,
             target: targetAlign,
             orient: orient
-        }]
-        if (opposit) {
-            res = res.concat(opposit(levitate, target, constraint))
         }
-        return res
     }
 }
 
@@ -139,13 +143,8 @@ const opposite: { [key: string]: HAlign | VAlign } = {
 
 for (let a1 of aligns) {
     for (let a2 of aligns) {
-        let o1 = opposite[a1]
-        let o2 = opposite[a2]
-
-        calcPlacementH[`${a1}-${a2}`] = placementCalculator(a1, a2, "H",
-            o1 && o2 ? placementCalculator(o1, o2, "H") : null)
-        calcPlacementV[`${a1}-${a2}`] = placementCalculator(a1, a2, "V",
-            o1 && o2 ? placementCalculator(o1, o2, "V") : null)
+        calcPlacementH[`${a1}-${a2}`] = placementCalculator(a1, a2, "H")
+        calcPlacementV[`${a1}-${a2}`] = placementCalculator(a1, a2, "V")
     }
 }
 
@@ -161,7 +160,6 @@ function getLevitatePosition(pA: Placement, pV: Placement, levitate: Rect, const
     rect = constraint.constraint(rect)
 
 
-
     // drawRect(rect, "green")
     // drawRect(constraint, "black")
     // drawRect(connect, "purple")
@@ -172,29 +170,5 @@ function getLevitatePosition(pA: Placement, pV: Placement, levitate: Rect, const
         Math.round(constraint.width),
         Math.round(constraint.height)
     )
-
-    // let res: LevitatePosition & { [key: string]: any } = {
-    //     maxWidth: Math.round(constraint.width),
-    //     maxHeight: Math.round(constraint.height),
-    //     left: pA.levitate === "right" ? Math.round(constraint.right) - Math.round(levitate.width) : Math.round(rect.left),
-    //     top: pV.levitate === "bottom" ? Math.round(constraint.bottom) - Math.round(levitate.height) : Math.round(rect.top),
-    //     anchor: {
-    //         position: {
-    //             align: {
-    //                 horizontal: pA.target as HAlign,
-    //                 vertical: pV.target as VAlign
-    //             }
-    //         },
-    //         levitate: {
-    //             align: {
-    //                 horizontal: pA.levitate as HAlign,
-    //                 vertical: pV.levitate as VAlign
-    //             }
-    //         }
-    //     }
-    // }
-
-
-    // return res
 }
 
