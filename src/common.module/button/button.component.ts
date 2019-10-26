b9ed08da67f38130b229b034aeb70d05600a6d19
Vetési Zoltán <vetesi.zoltan@gmail.com>
2019-10-26 16:31:55 +0200

    feat: button focus elevation

--- src/common.module/button/button.component.ts
+++ src/common.module/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Inject, Input, Output, EventEmitter, OnDestroy, OnInit, HostBinding, HostListener } from "@angular/core"
+import { Component, ElementRef, Inject, Input, Output, EventEmitter, OnDestroy, OnInit, HostBinding, HostListener, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core"
 import { FocusMonitor } from "@angular/cdk/a11y"
 
 import { AnzarComponent } from "../abstract-component"
@@ -26,6 +26,11 @@ export class ButtonComponent extends AnzarComponent implements OnDestroy, OnInit
     public ngOnInit() {
         this.destruct.subscription(this.focusMonitor.monitor(this.el.nativeElement)).subscribe((origin) => {
             this.focusOrigin = origin
+            if (origin) {
+                this.el.nativeElement.setAttribute("focused", origin)
+            } else {
+                this.el.nativeElement.removeAttribute("focused")
+            }
         })
         this.destruct.any(() => {
             this.focusMonitor.stopMonitoring(this.el.nativeElement)
