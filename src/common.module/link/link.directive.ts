de7d7a3f6b68be71cb82800ca70a291fa9d95bf6
Vetési Zoltán <zozzz@trigon.hu>
2021-03-24 00:30:19 +0100

    ...

--- src/common.module/link/link.directive.ts
+++ src/common.module/link/link.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, HostBinding, Inject } from "@angular/core"
-import { DomSanitizer } from "@angular/platform-browser"
+import { Directive, HostBinding, Inject, Input, OnChanges, SimpleChanges } from "@angular/core"
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser"
 
 
 @Directive({
     selector: ".nz-link"
 })
-export class LinkDirective {
-    @HostBinding("attr.href") public href = this.sanitizer.bypassSecurityTrustUrl("javascript:;")
+export class LinkDirective implements OnChanges {
+    @Input()
+    public href: string
+
+    @HostBinding("attr.href")
+    public _href: SafeUrl = this.sanitizer.bypassSecurityTrustUrl("javascript:;")
 
     public constructor(@Inject(DomSanitizer) private readonly sanitizer: DomSanitizer) {
 
+    }
+
+    public ngOnChanges(changes: SimpleChanges) {
+        if ("href" in changes) {
+            this._href = changes.href.currentValue
+        }
     }
 }
