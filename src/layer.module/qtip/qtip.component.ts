1d39f3ba26a2f4d3a9c3535dc1d7200e2d2476a0
Vetési Zoltán <zozzz@trigon.hu>
2021-04-10 18:30:21 +0200

    feat: qtip nl2br

--- src/layer.module/qtip/qtip.component.ts
+++ src/layer.module/qtip/qtip.component.ts
@@ -5,20 +5,27 @@ export const QTIP_TEXT = new InjectionToken("QTIP_TEXT")
 
 @Component({
     selector: "nz-qtip",
-    template: "{{ _text }}",
+    host: {
+        "[innerHtml]": "_htmlText"
+    },
+    template: "",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QtipComponent {
     public set text(val: string) {
         if (this._text !== val) {
             this._text = val
+            this._htmlText = val.replace(/\r?\n/g, "<br/>")
             this.cdr.markForCheck()
         }
     }
     public get text(): string { return this._text }
+    public _text: string
+    public _htmlText: any
 
     public constructor(
         @Inject(ChangeDetectorRef) protected readonly cdr: ChangeDetectorRef,
-        @Inject(QTIP_TEXT) public _text: string) {
+        @Inject(QTIP_TEXT) text: string) {
+        this.text = text
     }
 }
