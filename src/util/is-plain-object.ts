77bcdf4e7882af0f3321fa006c161208f2258b26
Zoltan Vetesi <vetesi.zoltan@gmail.com>
2026-07-21 22:43:23 +0200

    ...

--- src/util/is-plain-object.ts
+++ src/util/is-plain-object.ts
@@ -2,8 +2,5 @@ import * as isPlainObjectModule from "is-plain-object"
 
 type IsPlainObjectFn = (o: unknown) => boolean
 
-console.error("isPlainObjectModule", isPlainObjectModule)
-
 const mod = isPlainObjectModule as any
 export const isPlainObject: IsPlainObjectFn = (typeof mod === "function" ? mod : mod.isPlainObject || mod.default)!
-console.error("isPlainObject", isPlainObject({}))
